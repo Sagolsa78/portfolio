843084af28e7446fcf936b1d56248a1e072f6255
Mohit Sahani <sahanimohit5ed@gmail.com>
2025-05-29 01:05:07 +0530

    some changes have been made in desktop view layout.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,17 +2,13 @@ import { FaGithub, FaXTwitter } from "react-icons/fa6";
 import { CiLinkedin } from "react-icons/ci";
 import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from 'framer-motion';
-import {scrollToSection } from "./Navbar";
+import { scrollToSection } from "./Navbar";
 import { useMenu } from "./MenuContext";
-
-
-
 
 function Hero() {
   const words = ['Developer', 'Traveler', 'Photographer', 'Innovator'];
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const {menuOpen}=useMenu();
-
+  const { menuOpen } = useMenu();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -23,20 +19,35 @@ function Hero() {
   }, []);
 
   return (
-    <div className={`bg-black text-white px-6 py-20 mt-10 
+    <div className="bg-black text-white px-6 py-20 mt-20 lg:pt-30 lg:pb-30 md:mt-32 md:mb-32">
+      <div
+        className={`container mx-auto flex flex-col md:flex-row items-center justify-center gap-10 transition-all duration-300 ${
+          menuOpen ? "mt-38" : "mt-0"
+        }`}
+      >
+        {/* Image */}
+        <div className="flex justify-center md:justify-start">
+          <div className="rounded-full overflow-hidden lg:ml-10 border-4 border-pink-500 w-52 h-52 md:w-96 md:h-1/2">
+            <img
+              src="/My _PhotoGraph.jpeg"
+              alt="Profile"
+              className="object-cover w-full h-full"
+            />
+          </div>
+        </div>
 
-        }`}>
-      <div className={`container mx-auto flex flex-col-reverse md:flex-row items-center justify-between gap-10 flex-1 transition-all duration-300 ${menuOpen ? "mt-38" : "mt-0" }`}>
-        
         {/* Text content */}
-        <div className="flex-1 text-center md:text-left">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">
-            Hi, I'm <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">Mohit Sahani</span>
+        <div className="text-center lg:ml-24 md:text-left md:pl-10 flex-1">
+          <h1 className="lg:text-7xl text-4xl font-bold mb-4">
+            Hi, I'm 
+            <span className="bg-clip-text text-transparent bg-gradient-to-r ml-4 from-purple-500 to-pink-500">
+               Mohit Sahani
+            </span>
           </h1>
-          
-          <div className="text-2xl md:text-3xl mb-4">
-            I am a{" "}
-            <span className="inline-flex relative min-h-10 min-w-[150px] align-middle">
+
+          <div className="text-2xl md:text-4xl mb-4">
+            <span className="font-normal">I am a </span>
+            <span className="inline-flex relative min-h-10 lg:min-w-[250px] min-w-[150px] align-middle">
               <AnimatePresence mode="wait">
                 <motion.span
                   key={words[currentWordIndex]}
@@ -52,7 +63,7 @@ function Hero() {
             </span>
           </div>
 
-          <p className="text-lg text-gray-400 mb-8 max-w-xl mx-auto md:mx-0">
+          <p className="text-lg text-gray-400 mb-8 max-w-xl md:max-w-2xl mx-auto md:mx-0">
             Specializing in React, Node.js, and full-stack web development. Building real-time, scalable applications with a focus on intuitive UI/UX, WebSockets, and modern API integration.
           </p>
 
@@ -64,26 +75,15 @@ function Hero() {
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
             <a href="#projects" onClick={(e) => scrollToSection(e, "#Projects")}>
-              <button className="w-44 h-12 text-white font-semibold rounded-full bg-gradient-to-r from-purple-500 to-pink-500">
+              <button className="w-44 h-12 text-white  font-semibold rounded-full bg-gradient-to-r from-purple-500 to-pink-500">
                 Projects
               </button>
             </a>
             <a href="https://calendly.com/sahanimohit5ed/30min">
-              <button className="w-52 h-12 border border-purple-500 text-gray-300 font-semibold rounded-full">
+              <button className="w-52 h-12 border border-purple-500 text-gray-400 font-semibold rounded-full">
                 Schedule A Meet
               </button>
             </a>
-          </div>
-        </div>
-
-        {/* Image */}
-        <div className="flex-1 flex justify-center">
-          <div className="rounded-full overflow-hidden border-4 border-pink-500 w-52 h-52 md:w-96 md:h-96">
-            <img
-              src="/My _PhotoGraph.jpeg"
-              alt="Profile"
-              className="object-cover w-full h-full"
-            />
           </div>
         </div>
       </div>
